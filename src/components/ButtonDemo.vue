c27630a034c199d737dbd5b8982a950c1742a678
Promise-Devotion <sijun0224@msn.com>
2022-10-09 21:59:57 +0800

    创建button组件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,11 +1,16 @@
 <template>
-  <div>button</div>
+  <div>
+    <p>button</p>
+    <h2>例子</h2>
+    <div>
+      <Button>你好</Button>
+    </div>
+  </div>
 </template>
 <script lang="ts">
+import Button from "../lib/Button.vue";
 export default {
-  data() {
-    return {};
-  },
+  components: { Button },
 };
 </script>
 <style lang="scss" scoped></style>
