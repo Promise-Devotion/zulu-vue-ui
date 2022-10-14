d3ab9749b2c7999e6c20d8656f2d08c2af2b7e7a
Promise-Devotion <sijun0224@msn.com>
2022-10-14 21:33:41 +0800

    删除影响运行代码

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import { markdown } from "./plugins/markdown";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
   assetsDir: "assets",
-  plugins: [vue(), markdown()],
+  plugins: [vue()],
 });
